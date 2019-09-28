bb36c155d7b1048de63245c26c0355be9938ba9c
Kirill <48927672+KirillDaX@users.noreply.github.com>
2019-09-28 23:49:40 +0300

    Add files via upload

--- avia_scanner/app/views.py
+++ avia_scanner/app/views.py
@@ -21,16 +21,21 @@ def ticket_page_view(request):
 def cities_lookup(request):
     """Ajax request предлагающий города для автоподстановки, возвращает JSON"""
 
-    part_of_city = request.GET.get('term')
+    part_of_city = request.GET.get('term')  # получаем введеную подстроку
     pattern = f'^{part_of_city}.+'
-    all_cities = list(City.objects.values('name'))
+
     city_list = []
-    if not cache.get('all_cities'):
+    if not cache.get('all_cities'):  # проверяем есть ли кэш, если нет(первый запуск) создаем и вносим туда все данные
+        all_cities = list(City.objects.values('name'))  # запрашиваем сразу все, чтобы потом каждый раз не дергать базу
         cache.set('all_cities', all_cities, 300)
-    else:
+    else:  # если кеш есть ищем в нем (результат либо в нем, либо его нет в базе)
         for city in cache.get('all_cities'):
             match = re.findall(pattern, city['name'])
             if match:
                 city_list.append(city['name'])
+
+    if len(city_list) == 0:
+        city_list.append('нет совпадений')
+
     results = city_list
     return JsonResponse(results, safe=False)
